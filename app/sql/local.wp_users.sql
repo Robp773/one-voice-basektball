c90147ac7f1e25ee6a57cf5f54630a546c73d37d
Rob <robpeterman573@gmail.com>
2018-12-21 20:59:57 -0500

    misc wordpress updates

--- app/sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"RoBp773","$P$B.1NmLn17DFmLJRRQ6ZuPd4lcEP5Ku0","robp773","dev-email@flywheel.local","","2018-12-10 22:48:04","",0,"RoBp773");
